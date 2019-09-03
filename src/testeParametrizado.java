355a00c28d313b49c769c9885de9b2ac0ae167f3
Tâmara Tavares <tamara-bt@hotmail.com>
2019-09-03 11:05:53 -0300

    alterando para Junit 4

--- src/testeParametrizado.java
+++ src/testeParametrizado.java
@@ -3,10 +3,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 import java.util.Arrays;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 @RunWith(Parameterized.class)
-class testeParametrizado{
+public class testeParametrizado{
 	double valorEsperado;
 	Contribuinte c;
 	
@@ -20,22 +20,30 @@ class testeParametrizado{
 	public static Iterable getParameters() {
 		Object [][] parametros = new Object[][] {
 			{0, 
-			new Contribuinte("Fulano", new Rendimento[] {new Rendimento(1200,"bolsa"),new Rendimento(2000,"salario")},
+			new Contribuinte("Fulano", new Rendimento[] {new Rendimento(1200,"bolsa"),new Rendimento(200,"salario")},
+			new Deducao[] {new Deducao(100,"pensao"),new Deducao(190,"dependente")})
+			},
+			
+			{22.4415, 
+			new Contribuinte("Ciclano", new Rendimento[] {new Rendimento(1200,"bolsa"),new Rendimento(2000,"salario")},
 			new Deducao[] {new Deducao(1000,"pensao"),new Deducao(190,"dependente")})
 			},
 			
-			{0, 
-			new Contribuinte("Ciclano", new Rendimento[] {new Rendimento(1200,"bolsa"),new Rendimento(2000,"salario")},
+			{22.4415, 
+			new Contribuinte("Beltrano", new Rendimento[] {new Rendimento(1200,"bolsa"),new Rendimento(3000,"salario")},
 			new Deducao[] {new Deducao(1000,"pensao"),new Deducao(190,"dependente")})
-			}};
+			},
+			
+		};
 		
 		return Arrays.asList(parametros);
 		}
 	
 	@Test
-	void testeComParametros() throws BaseInvalidaException {
+	public void testeComParametros() throws BaseInvalidaException {
 		CalculosIRPF calculo = new CalculosIRPF();
 		double imposto = calculo.calculaImposto(this.c);
+		System.out.println(imposto);
 		assertEquals(this.valorEsperado,imposto,0);
 	}
 
